5954c4f7ced555802859f81a45f19eb689ee645a
vincent <vincent@clickinglabs.com>
2012-06-07 14:32:39 +0800

    Now fetches emails from search results. Accepts the number of results as parameters

--- google_fetch.rb
+++ google_fetch.rb
@@ -1,25 +1,37 @@
 require 'nokogiri'
 require 'open-uri'
 require 'anemone'
+require 'uri'
 
 class GoogleFetch
-  def initialize(tags)
+  def initialize(tags, num_of_pages)
     @tags = tags
+    @pages = num_of_pages
   end
 
   def start
     clean_tag = @tags.gsub(" ", "+");
-    url = "http://www.google.com/search?num=100&q=#{clean_tag}"
+    url = "http://www.google.com/search?num=#{@pages}&q=#{clean_tag}"
     result = Nokogiri::HTML(open(url))
     
-	File.open('output.txt', 'w') do |f|
+    File.open('output.txt', 'w') do |f|
       result.css('h3.r a').each do |link|
-	    f.puts link['href'].to_s
-        Anemone.crawl("http://" + link['href'].to_s) do |website|
-		  website.on_every_page { |p| f.puts p.doc.at('title') rescue nil }
-		end
-      end
+        host = URI.parse(link['href'].to_s).host
+        f.puts host + " :\n"
+        Anemone.crawl("http://" + host) do |website|
+	  website.on_every_page do |page|
+            r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+            begin
+              emails = "#{page.doc.at('body')}".scan(r).uniq
+              f.puts emails
+            rescue
+              f.puts "can't find email here."
+            end
+          end
+          #website.on_every_page { |p| f.puts "#{p.doc.at('title')} : #{p.url} #{p.doc.at('body')} : #{host} \n==========================\n" rescue nil }
 	end
+      end
+    end
 
   end
 end
@@ -27,6 +39,5 @@ end
 class String
   def to_s
    str = self.gsub('/url?q=','')
-   str.match(/\/\/([^"]*)\&sa/).nil? ? str : str.match(/\/\/([^"]*)\&sa/)[1]
   end
 end
